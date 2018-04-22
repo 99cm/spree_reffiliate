57ab83a80ec98bb4810f154bfe075dae582ce1da
99cm <zyizyao@china-guide.com>
2018-04-22 16:45:14 -0400

    Update rails 5.1 activerecord migrate support

--- db/migrate/20140913042034_add_layout_to_spree_affiliate.rb
+++ db/migrate/20140913042034_add_layout_to_spree_affiliate.rb
@@ -1,4 +1,4 @@
-class AddLayoutToSpreeAffiliate < ActiveRecord::Migration
+class AddLayoutToSpreeAffiliate < ActiveRecord::Migration[5.1]
   def change
     add_column :spree_affiliates, :layout, :string
   end
