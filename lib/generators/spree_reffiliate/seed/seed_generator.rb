f64cfe816735fd7d17a0c4330cd57630f7182d90
99cm <zyizyao@china-guide.com>
2018-04-22 12:50:40 -0400

    Update seed_generator.rb

--- lib/generators/spree_reffiliate/seed/seed_generator.rb
+++ lib/generators/spree_reffiliate/seed/seed_generator.rb
@@ -1,10 +1,10 @@
 module SpreeReffiliate
   module Generators
     class SeedGenerator < Rails::Generators::Base
-      source_root File.expand_path("../../templates", FILE)
+      source_root File.expand_path("../../templates", _FILE_)
 
       def run_db_seeds
-        seed_file = File.join(File.expand_path("../../../../../db", FILE), "seeds.rb")
+        seed_file = File.join(File.expand_path("../../../../../db", _FILE_), "seed.rb")
         load(seed_file) if File.exist?(seed_file)
       end
     end
