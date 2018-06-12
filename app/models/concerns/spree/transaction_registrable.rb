2fcd4265dd43f1900ca69d76405241d993d2991b
99cm <zyizyao@china-guide.com>
2018-06-12 14:41:31 -0400

    Update transaction_registrable.rb

--- app/models/concerns/spree/transaction_registrable.rb
+++ app/models/concerns/spree/transaction_registrable.rb
@@ -8,7 +8,7 @@ module Spree::TransactionRegistrable
       end
 
       def affiliate_commission_rule_present?(affiliate)
-        if self.class.eql? Spree::User
+        if self.class.eql? User
           commission_rule_id = Spree::CommissionRule.user_registration.id.try(:id)
         elsif self.class.eql? Spree::Order
           commission_rule_id = Spree::CommissionRule.order_placement.id.try(:id)
