4fbbe0b5b51b2fd8f6a86c84559cde90bbfc7832
99cm <zyizyao@china-guide.com>
2018-06-12 17:09:11 -0400

    Change to factory_bot

--- spec/models/spree/promotion/rules/referred_promotion_rule_spec.rb
+++ spec/models/spree/promotion/rules/referred_promotion_rule_spec.rb
@@ -2,19 +2,19 @@ require 'spec_helper'
 
 describe Spree::Promotion::Rules::ReferredPromotionRule, type: :model do
   before(:each) do
-    @rule = FactoryGirl.build(:referred_promotion_rule)
-    @user = FactoryGirl.create(:user, email: FFaker::Internet.email)
-    @referred = FactoryGirl.create(:user, email: FFaker::Internet.email, referral_code: @user.referral.code)
-    @organic_order = FactoryGirl.create(:order, user: @user)
-    @referred_order = FactoryGirl.create(:order, user: @referred)
+    @rule = FactoryBot.build(:referred_promotion_rule)
+    @user = FactoryBot.create(:user, email: FFaker::Internet.email)
+    @referred = FactoryBot.create(:user, email: FFaker::Internet.email, referral_code: @user.referral.code)
+    @organic_order = FactoryBot.create(:order, user: @user)
+    @referred_order = FactoryBot.create(:order, user: @referred)
   end
-  it "is applicable for an order" do
+  it 'is applicable for an order' do
     expect(@rule.applicable?(@referred_order)).to be_truthy
   end
-  it "is elegible for referred user" do
+  it 'is elegible for referred user' do
     expect(@rule.eligible?(@referred_order)).to be_truthy
   end
-  it "is not elegible for organic user" do
+  it 'is not elegible for organic user' do
     expect(@rule.eligible?(@organic_order)).to be_falsy
   end
 end
